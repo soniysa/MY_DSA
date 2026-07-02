885da8f2f47ad02b215c932e4da69ecef192c731
soniysa <yashsony990099@gmail.com>
2026-07-02 14:25:48 +0530

    Time: 0 ms (100%), Space: 19.2 MB (96.69%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,23 @@
+class Solution:
+
+    def solve(self, nums: List[int], permutation, myset, result):
+
+        if len(permutation) == len(nums):
+            result.append (list(permutation))
+            return
+
+
+        for i in range(0, len(nums)):
+            if nums[i] not in myset:
+                myset.add(nums[i])
+                permutation.append(nums[i])
+                self.solve(nums, permutation, myset, result)
+                myset.remove(nums[i])
+                permutation.remove(nums[i])
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        permutation = []
+        myset = set()
+        self.solve(nums, permutation, myset, result)
+        return result
