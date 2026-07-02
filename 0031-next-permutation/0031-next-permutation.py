503700bfdac43104c91fbb90fb20de08dc420777
soniysa <yashsony990099@gmail.com>
2026-07-02 16:18:17 +0530

    Time: 0 ms (100%), Space: 19.4 MB (26.06%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,10 +1,10 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-
-        for i in range(len(nums) - 1, 0, -1):
-
-            if nums[i - 1] < nums[i]:
-
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range (len(nums)-1, 0, -1):
+            if nums[i-1] < nums[i]:
                 nums[i:] = sorted(nums[i:])
 
                 j = i - 1
@@ -12,6 +12,5 @@ class Solution:
                 for k in range(i, len(nums)):
                     if nums[j] < nums[k]:
                         nums[j], nums[k] = nums[k], nums[j]
-                        return
-
+                        return 
         nums.reverse()
