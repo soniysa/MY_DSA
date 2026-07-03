67ad6e08ed5491d03388d4db81f03484534384a0
soniysa <yashsony990099@gmail.com>
2026-07-03 15:52:16 +0530

    Time: 3 ms (34.8%), Space: 19.2 MB (79.04%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,27 +1,37 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        s = s.strip()  # Remove leading/trailing spaces
-        if not s:
+
+        s = s.strip()
+
+        if not s :
             return 0
 
-        sign, i, res = 1, 0, 0
+        sign = 1
+        ans = 0
+        idx = 0 
+ 
+        if s[0] == '-' or s[0] == '+' :
+            if s[0] == '-' :
+                sign = -1
+            idx+=1
 
-        # Check for sign
-        if s[0] == '-':
-            sign = -1
-            i += 1
-        elif s[0] == '+':
-            i += 1
+        while idx < len(s) and s[idx].isdigit():
+            digit = int(s[idx])
 
-        while i < len(s) and s[i].isdigit():
-            res = res * 10 + int(s[i])
+            if sign == 1 and (
+                ans > 2147483647 // 10 or
+                (ans == 2147483647 // 10 and digit > 7)
+            ):
+                return 2147483647
+            
+            if sign == -1 and (
+                ans > 2147483648 // 10 or
+                (ans == 2147483648 // 10 and digit > 8)
+            ):
+                return -2147483648
+            
+            ans = ans * 10 + digit 
+            idx += 1
 
-            # Handle overflow
-            if sign * res > 2**31 - 1:
-                return 2**31 - 1
-            if sign * res < -2**31:
-                return -2**31
-
-            i += 1
-
-        return sign * res
+        return sign * ans
+        
