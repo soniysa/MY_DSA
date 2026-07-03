e81d75884c3ef9755d0b628464123025d030f76e
soniysa <yashsony990099@gmail.com>
2026-07-03 15:49:53 +0530

    Time: 3 ms (34.8%), Space: 19.4 MB (13.33%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -1,41 +1,37 @@
 class Solution:
-    def myAtoi(self, s: str):
+    def myAtoi(self, s: str) -> int:
 
         s = s.strip()
 
-        if not s:
+        if not s :
             return 0
 
         sign = 1
         ans = 0
-        idx = 0
-
-        # Check sign
-        if s[0] == '-' or s[0] == '+':
-            if s[0] == '-':
+        idx = 0 
+ 
+        if s[0] == '-' or s[0] == '+' :
+            if s[0] == '-' :
                 sign = -1
-            idx += 1
+            idx+=1
 
-        # Read digits
         while idx < len(s) and s[idx].isdigit():
-
             digit = int(s[idx])
 
-            # Overflow check for positive number
-            if sign == 1:
-                if ans > 2147483647 // 10 or (
-                    ans == 2147483647 // 10 and digit > 7
-                ):
-                    return 2147483647
-
-            # Overflow check for negative number
-            else:
-                if ans > 2147483648 // 10 or (
-                    ans == 2147483648 // 10 and digit > 8
-                ):
-                    return -2147483648
-
-            ans = ans * 10 + digit
+            if sign == 1 and (
+                ans > 2147483647 // 10 or
+                (ans == 2147483647 // 10 and digit > 7)
+            ):
+                return 2147483647
+            
+            if sign == -1 and (
+                ans > 2147483648 // 10 or
+                (ans == 2147483648 // 10 and digit > 8)
+            ):
+                return -2147483648
+            
+            ans = ans * 10 + digit 
             idx += 1
 
         return sign * ans
+        
