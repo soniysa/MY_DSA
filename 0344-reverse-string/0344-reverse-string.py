cc0dff255673fc8f8d78c04f37125e2f109ccc0c
soniysa <yashsony990099@gmail.com>
2026-07-05 15:51:24 +0530

    Time: 7 ms (10.39%), Space: 23.4 MB (80.05%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,4 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        s.reverse()
+        first = 0
+        last = len(s)-1
+
+        while first <= last:
+            s[first], s[last] = s[last], s[first]
+
+            first += 1
+            last -= 1
